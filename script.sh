8b725e0441abe997d3dbfde9933fedc3194cece0
sumedhreddy90 <sumedhreddykoppula@gmail.com>
2021-10-15 11:54:52 -0400

    (task 1.4) Creating .sh bash file for Yolo weights, config and cocodatasets

---
+++ script.sh
@@ -0,0 +1,6 @@
+
+wget https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
+
+wget https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov3.cfg
+
+wget https://pjreddie.com/media/files/yolov3.weights
