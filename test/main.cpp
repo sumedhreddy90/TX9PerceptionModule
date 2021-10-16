acf4986ce54a232f2c1b351a55b173eb88001b1d
sumedhreddy90 <sumedhreddykoppula@gmail.com>
2021-10-16 18:21:37 -0400

    [Task 1.19] Update CmakeLists.txt in tests

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,9 @@
+/**
+ * @copyright  MIT License (c) 2021 Pratik Acharya and Sumedh Koppula
+ * @file  main.cpp
+ * @brief Tests for test main method
+ * @author Pratik Acharya and Sumedh Koppula
+ */
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
