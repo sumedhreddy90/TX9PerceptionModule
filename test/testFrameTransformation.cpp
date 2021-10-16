1c3a85c426d0b71080c5505d7bb67001a3a4f874
Pratik Acharya <pratik99@umd.edu>
2021-10-16 17:20:49 -0400

    [Task 1.17]Created test for FrameTransformation class

---
+++ test/testFrameTransformation.cpp
@@ -0,0 +1,29 @@
+/**
+ * @copyright  MIT License (c) 2021 Pratik Acharya and Sumedh Koppula
+ * @file  testFrameTransformation.cpp
+ * @brief Tests for FrameTransformation class
+ * @author Pratik Acharya and Sumedh Koppula
+ */
+
+#pragma once
+#include <gtest/gtest.h>
+#include <FrameTransformation.hpp>
+
+FrameTransformation frameTransformation;
+vector<vector<double>> testMatrix { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+vector<double> testVector { 1, 2, 3 };
+
+/**
+ * @brief test for getTransformationMatrix() and setTransformationMatrix() methods
+ */
+TEST_F(testGetSet, testTransformationMatrix) {
+  frameTransformation.setTransformationMatrix(testMatrix);
+  EXPECT_EQ(frameTransformation.getTransformationMatrix(), testMatrix);
+}
+
+/**
+ * @brief test for transformFrame() method
+ */
+TEST_F(testTransformation, testTransformFrame) {
+  EXPECT_EQ(frameTransformation.transformFrame(testVector), testMatrix);
+}
